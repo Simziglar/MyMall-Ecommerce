0b1b68e8f67179b767d158b45d84d8cb7e3809fc
Simziglar <60963037+Simziglar@users.noreply.github.com>
2022-05-06 20:09:12 +0300

    Created SignUpActivity.java and activity_sign_up.xml

--- app/src/main/java/com/example/mymallecommerce/LogInActivity.java
+++ app/src/main/java/com/example/mymallecommerce/LogInActivity.java
@@ -2,13 +2,26 @@ package com.example.mymallecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
+    private TextView signInText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        signInText = findViewById(R.id.signInText);
+        signInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
